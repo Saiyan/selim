abfe0796800ac54e63c1385e40e26f52b5ab9ca9
Saiyan <saiyan@cssai.eu>
2014-12-16 11:20:31 +0100

    add Test for Util::findInArrayWithRegex

--- tests/UtilTest.php
+++ tests/UtilTest.php
@@ -38,4 +38,38 @@ class UtilTests extends PHPUnit_Framework_TestCase {
         $si = Util::filterSitesByName($sites, $filter_positivelookahead);
         $this->assertEquals(2, count($si));
     }
+
+    function testFindInArrayWithRegex(){
+        $arr = array(
+            'name1',
+            'name2',
+            'sitename3',
+            'sitename4',
+            'testFIVE',
+            'testSIX',
+        );
+
+        $filter_startswith_name = "/^name/";
+        $si = Util::findInArrayWithRegex($arr, $filter_startswith_name);
+        $this->assertEquals($arr[0], $si);
+
+
+        $filter_name2 = "/name2/";
+        $si = Util::findInArrayWithRegex($arr, $filter_name2);
+        $this->assertEquals($arr[1], $si);
+
+
+        $filter_endswithdecimal = "/\\d$/";
+        $si = Util::findInArrayWithRegex($arr, $filter_endswithdecimal);
+        $this->assertEquals($arr[0], $si);
+
+
+        $filter_positivelookbehind = "/(?<=te)\\w+/";
+        $si = Util::findInArrayWithRegex($arr, $filter_positivelookbehind);
+        $this->assertEquals($arr[2], $si);
+
+        $filter_positivelookahead = "/te(?=na)/";
+        $si = Util::findInArrayWithRegex($arr, $filter_positivelookahead);
+        $this->assertEquals($arr[2], $si);
+    }
 }
