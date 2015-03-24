e9dd2a745af80ff06ede5b805cb9efd200f793d3
Saiyan <saiyan@cssai.eu>
2015-03-24 13:12:23 +0100

    fix phpunit autoload problem

--- src/autoload.php
+++ src/autoload.php
@@ -4,17 +4,17 @@ class AutoLoader
 {
     public static function loadClass()
     {
-        require_once '../vendor/autoload.php';
-        require_once '../vendor/pear/console_table/Table.php';
-        require_once 'classes/Output.php';
-        require_once 'classes/IOutput.php';
-        require_once 'classes/ConsoleOutput.php';
-        require_once 'classes/ConsoleOutputTable.php';
-        require_once 'classes/SilverstripePage.php';
-        require_once 'classes/SiteConfig.php';
-        require_once 'classes/Util.php';
-        require_once 'classes/SelimConfig.php';
-        require_once 'classes/SelimCLI.php';
-        require_once 'classes/SecurityChecker.php';
+        require_once __DIR__.'/../vendor/autoload.php';
+        require_once __DIR__.'/../vendor/pear/console_table/Table.php';
+        require_once __DIR__.'/classes/Output.php';
+        require_once __DIR__.'/classes/IOutput.php';
+        require_once __DIR__.'/classes/ConsoleOutput.php';
+        require_once __DIR__.'/classes/ConsoleOutputTable.php';
+        require_once __DIR__.'/classes/SilverstripePage.php';
+        require_once __DIR__.'/classes/SiteConfig.php';
+        require_once __DIR__.'/classes/Util.php';
+        require_once __DIR__.'/classes/SelimConfig.php';
+        require_once __DIR__.'/classes/SelimCLI.php';
+        require_once __DIR__.'/classes/SecurityChecker.php';
     }
 }
