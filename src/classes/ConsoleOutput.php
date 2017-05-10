ed78a66dabc863c12113136d72f83d8cb69e89e2
Saiyan <saiyan@cssai.eu>
2017-05-10 17:02:10 +0200

    templating start

--- src/classes/ConsoleOutput.php
+++ src/classes/ConsoleOutput.php
@@ -2,15 +2,44 @@
 
 namespace Selim;
 
+use Symfony\Component\Templating\PhpEngine;
+use Symfony\Component\Templating\TemplateNameParser;
+use Symfony\Component\Templating\Loader\FilesystemLoader;
+
 class ConsoleOutput extends Output implements IOutput
 {
+
     public function write($format = '')
     {
         foreach ($this->pages as $sspage) {
             if ($sspage instanceof SilverstripePage) {
-                echo self::formatSSpage($sspage, $format);
+                //echo self::formatSSpage($sspage, $format);
+                echo self::render($sspage);
             }
         }
+    }
+
+    private static function render(SilverstripePage $sspage){
+        $loader = new FilesystemLoader(__DIR__.'/../views/%name%');
+
+        $templating = new PhpEngine(new TemplateNameParser(), $loader);
+
+        echo $templating->render('default_default.php', array('firstname' => 'Fabien'));
+
+        $values = array(
+            "newline" => PHP_EOL,
+            "name" => $sspage->getName(),
+            "version" => $sspage->getVersion(),
+            "daText" => $sspage->hasDefaultAdmin(),
+            "elText" => $sspage->hasEmailLogging(),
+            "etText" => $sspage->getEnvironmentType(),
+            "modules" =>  $sspage->getModules(),
+            "cfg_php_path" => $sspage->getConfigPhpPath(),
+            "cfg_yaml_path" => $sspage->getConfigYmlPath(),
+            "root_path" => $sspage->getRootPath(),
+        );
+
+        echo $templating->render('default_default.php', $values);
     }
 
     private static $format_default = "Site:            %s%nVersion:         %v%nDefaultAdmin:    %da%nEmailLogging:    %el%nEnvironmentType: %et%nModules:         %mo%n%n";
---
+++ src/views/default_default.php
@@ -0,0 +1,7 @@
+Site:            <?php echo $name ?> <?php echo $newline ?>
+Version:         <?php echo $version ?> <?php echo $newline ?>
+DefaultAdmin:    <?php echo $daText ?> <?php echo $newline ?>
+EmailLogging:    <?php echo $elText ?> <?php echo $newline ?>
+EnvironmentType: <?php echo $etText ?> <?php echo $newline ?>
+Modules:
+<?php echo $newline ?><?php echo $newline ?><?php echo $newline ?>
