6f31a62cc54d603a8a08aea39d207cdc212b18f6
Saiyan <saiyan@cssai.eu>
2017-05-11 11:52:26 +0200

    add "Generated" string to head

--- src/views/default_html.js
+++ src/views/default_html.js
@@ -3,7 +3,7 @@ var moduleLinks = document.querySelectorAll('.module-toggle');
 Array.prototype.forEach.call(moduleLinks, function(el, i){
     el.onclick = function(event) {
         event.preventDefault();
-        
+
         var listEle = document.getElementById(el.getAttribute('data-ListID'));
         listEle.classList.toggle('active');
 
